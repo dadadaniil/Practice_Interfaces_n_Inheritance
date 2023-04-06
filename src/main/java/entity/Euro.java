bf83261bc1c604385bfe970608cfd367207ec4aa
Daniil Anishchanka <den382505@gmail.com>
2023-04-06 22:12:20 +0300

    looks like Euro class is done

--- src/main/java/entity/Euro.java
+++ src/main/java/entity/Euro.java
@@ -17,4 +17,22 @@ public class Euro {
     public void setCents(int cents) {
         this.cents = cents;
     }
+
+
+    public String centsToEuros(int cents) {
+        return String.format("%d.%02d", cents / 100, cents % 100);
+    }
+
+    public boolean equals(Euro euros) {
+        return this.cents == euros.getCents();
+    }
+
+    public int compareTo(Euro euros) {
+        return this.cents - euros.getCents();
+    }
+
+    @Override
+    public String toString() {
+        return centsToEuros(cents);
+    }
 }
