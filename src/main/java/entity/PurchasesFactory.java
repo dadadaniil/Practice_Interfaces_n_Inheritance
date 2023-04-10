b86f28907200b56c1cd646c91ce1a2f45e296a75
Alena Tsibets <alena.tsibets@gmail.com>
2023-04-10 21:10:16 +0300

    fixed PurchasesFactory

--- src/main/java/entity/PurchasesFactory.java
+++ src/main/java/entity/PurchasesFactory.java
@@ -4,16 +4,30 @@ import java.util.Scanner;
 
 public class PurchasesFactory {
     private enum PurchaseKind {
-        GENERAL_PURCHASE, PURCHASE_WITH_DISCOUNT, PURCHASE_WITH_PROPORTIONAL_DISCOUNT
+        GENERAL_PURCHASE {
+            @Override
+            Purchase getPurchase(Scanner sc) {
+                return new Purchase(sc);
+            }
+        },
+        PURCHASE_WITH_DISCOUNT{
+            @Override
+            Purchase getPurchase(Scanner sc) {
+                return new DiscountPerUnitPurchase(sc);
+            }
+        },
+        PURCHASE_WITH_PROPORTIONAL_DISCOUNT{
+            @Override
+            Purchase getPurchase(Scanner sc) {
+                return new ProportionalDiscountPurchase(sc);
+            }
+        };
+        abstract Purchase getPurchase(Scanner sc);
     }
 
     public static Purchase getPurchaseFromFactory(Scanner sc) {
         String id = sc.next();
         PurchaseKind kind = PurchaseKind.valueOf(id);
-        return switch (kind) {
-            case GENERAL_PURCHASE -> new Purchase(sc);
-            case PURCHASE_WITH_DISCOUNT -> new DiscountPerUnitPurchase(sc);
-            case PURCHASE_WITH_PROPORTIONAL_DISCOUNT -> new ProportionalDiscountPurchase(sc);
-        };
+        return kind.getPurchase(sc);
     }
 }
